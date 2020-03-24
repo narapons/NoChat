b26a8a7dcaec19f745a7df307cea3211116fd872
narapon <43005911+narapons@users.noreply.github.com>
2020-03-24 14:35:40 +0900

    誤字修正

--- src/NoChat/NoChat.php
+++ src/NoChat/NoChat.php
@@ -58,7 +58,7 @@ class NoChat extends PluginBase implements Listener
                             break;
                         }
                         if (!$this->list->exists($args[1])) {
-                            $sender->sendmessage("[Nochat] §c{$args[1]}はチャットを制限されていません");
+                            $sender->sendmessage("[Nohat] §c{$args[1]}はチャットを制限されていません");
                             break;
                         }
                         $sender->sendmessage("[NoChat] §e{$args[1]}のチャットの使用の制限を解除しました");
@@ -99,7 +99,7 @@ class NoChat extends PluginBase implements Listener
             case "me":
             case "say":
                 if ($this->list->exists($event->getSender()->getName())) {
-                    $event->getSender()->sendmessage("§cあなたは-チャットの使用を制限されています");
+                    $event->getSender()->sendmessage("§cあなたはチャットの使用を制限されています");
                     $event->setCancelled();
                 }
                 break;
